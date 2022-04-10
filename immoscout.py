313dd2c16fba6175fa31eb708f4609b088672392
phn2211 <phn2211@gmail.com>
2022-04-10 19:13:50 +0200

    kleinanzeigen

--- immoscout.py
+++ immoscout.py
@@ -52,7 +52,7 @@ soup = BeautifulSoup(page, "html.parser")
 srchRsltsContent = soup.find("ul", id="srchrslt-adtable")
 
 # Setzt die Einträge inheralb der Seach-Results und dem Table dortdrinn in eine Variable / Array.
-srchRslts = soup.find_all(class_="article")
+srchRslts = soup.find_all("article")
 
 # Setzt einen Counter.
 counter = 0
